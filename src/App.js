e3acb2055d4e5585e7810ff37f6bf32a29172fec
Thenu <anbunachiappan@gmail.com>
2023-05-01 14:32:27 -0700

    initial

---
+++ src/App.js
@@ -0,0 +1,23 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+      <div>
+        <h1>Tic-tac-toe</h1>
+        <p>dgfdgdfgdfgdfgfdgfgfdfdgfd</p>
+        <div>3*3 box</div>
+      {
+        <div></div>
+      }
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
