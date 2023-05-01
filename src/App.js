0b0ee3bb4bf70381b9cdbd9cd3904726e0ff2fe6
Thenu <anbunachiappan@gmail.com>
2023-05-01 15:25:12 -0700

    working matrix

--- src/App.js
+++ src/App.js
@@ -11,13 +11,31 @@ function App() {
         <h1>Tic-tac-toe</h1>
         <p>dgfdgdfgdfgdfgfdgfgfdfdgfd</p>
         <div>3*3 box</div>
+     <div className='matrix'>
       {
-        <div></div>
+        <div className='eachElementRC'>{matrix(3,3)}</div>
       }
+     </div>
 
       </div>
     </div>
   );
 }
-
+const matrix = (rowCount,colCount) =>{
+  let arr =[];
+  for (let i=0;i<rowCount;i++){
+    let row = [];
+    for (let j=0;j<colCount;j++){
+      let col ="0" //pushing empty string
+      row.push(col)
+    }
+    arr.push(row)
+    console.log(arr,"arr")
+  }
+  return arr
+}
+// let matrix = Array(3).fill(0).map((row,index) =>      
+//     new Array(3).fill('Row ' + (index+1)) 
+// )
+console.log(matrix(3,3))
 export default App;
